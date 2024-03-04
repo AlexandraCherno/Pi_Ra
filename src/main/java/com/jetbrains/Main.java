585151782bdbfeafc56791a5d88f1d5dbc9d457b
AlexandraCherno <Alexandra.Chernova@urfu.me>
2024-03-04 21:34:05 +0500

    Ver 2.0

--- src/main/java/com/jetbrains/Main.java
+++ src/main/java/com/jetbrains/Main.java
@@ -1,9 +1,27 @@
 package com.jetbrains;
+import org.eclipse.jetty.server.Server;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
 public class Main {
-    public static void main(String[] args) {
-System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
     }
 }
